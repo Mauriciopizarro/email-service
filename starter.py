c308aa08487a89b434aadacae7668caaec45ea21
Mauri <pizarromauricio031096@gmail.com>
2024-04-27 13:24:34 -0300

    fix-imports-consumers

--- starter.py
+++ starter.py
@@ -1,6 +1,8 @@
-from infrastructure.events.consumers.reset_pass_send_email import PassResetSendEmailListener
-from infrastructure.events.consumers.user_created_send_email import UserCreatedSendEmailListener
+import time
 from threading import Thread
+from infrastructure.events.consumers.reset_pass_send_email import PassResetSendEmailListener
+from infrastructure.events.consumers.reset_password_code_verification_email import ResetPassVerificationCodelListener
+from infrastructure.events.consumers.user_created_send_email import UserCreatedSendEmailListener
 
 
 def start():
